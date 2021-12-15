41b9e44c7e44ed0a4bef77255dec2e98186b909e
Sarfraaz Ahmed <sarfraaz@gmail.com>
2021-12-15 11:42:36 +0530

    Make display() a method of Date class
    Move "display()" function inside the Date class to make it a method.
    Also notice the change in invoking the function "display()" as compared to the way of invoking "display()" as a method on a Date object

--- python3_code/date.py
+++ python3_code/date.py
@@ -13,22 +13,25 @@ class Date:
     mmm = 1
     yyy = 1970
 
+    def display(val):
+        print(val.ddd)
+        print(val.mmm)
+        print(val.yyy)
+
 ######################
-
-# Below code is outside of class Date
-def display(val):
-    print(val.ddd)
-    print(val.mmm)
-    print(val.yyy)
-
 # Main or client code starts here
 start = Date()
-display(start)
+#display(start)
+
+# If display() was a method 
+# How would I invoke it on "start" instance ?
+start.display()
 
 end = Date()
 end.ddd = 31
 end.mmm = 12
 end.yyy = 2021
-display(end)
+#display(end)
+end.display()
 
 # Copyright 2021 Sarfraaz Ahmed. All rights reserved.
